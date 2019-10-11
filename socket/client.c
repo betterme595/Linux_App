2ec6594d855f349ed75760ec4f521d584c2e7638
luyingjian <luyj595@163.com>
2019-10-11 16:51:04 +0800

    上传client代码,但存在无法实时通信问题,待解决

---
+++ socket/client.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+	int sock = 0;
+	struct sockaddr_in serv_addr;
+	printf("[%s]-[%d]\n", __func__, __LINE__);
+
+	if(argc != 3){
+		printf("Usage: %s [ip] [port]\n",argv[0]);
+	}
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock < 0){
+		perror("socket");
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(atoi(argv[2]));		//端口
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]); //IP地址
+//	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); //IP地址
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+		perror("connect");
+		return -1;
+	}
+	char buf[]="hello world\n";
+//	while(1){
+		printf("client send\n");
+		write(sock, buf, sizeof(buf));
+		fsync(sock);
+//		flush(sock);
+		sleep(1);
+//	}
+	close(sock);
+	return 0;
+}
--- socket/server.c
+++ socket/server.c
@@ -27,6 +27,12 @@ static int InitSocket(int *socket_fd_p)
 
 //	serv_addr.sin_family = AF_INET;
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);//创建ipv4的socket,protocol=0:自动选择
+	if( PROCESS_ERR == socket_fd ){
+		printf("socket_fd:[%d]\n",socket_fd);
+		return PROCESS_ERR;
+	}
+
+	*socket_fd_p = socket_fd;
 
 	bind(socket_fd,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
 
@@ -36,6 +42,10 @@ static int InitSocket(int *socket_fd_p)
 int main(int argc, char *argv[])
 {
 	int server_socket_fd;
+	int read_size = 0;
+	char read_buf[1024];
+	char write_buf[]="test send";
+
 	struct sockaddr_in client_addr;
 	printf("[%s]-[%d]\n", __func__, __LINE__);
 	InitSocket(&server_socket_fd);
@@ -45,6 +55,19 @@ int main(int argc, char *argv[])
 	socklen_t client_addr_size = sizeof(client_addr);
 
 	int client_socket_fd = accept(server_socket_fd, (struct sockaddr*)&client_addr, &client_addr_size);
+	if( -1 == client_socket_fd){
+		printf("client_socket_fd:[%d]\n",client_socket_fd);
+	}
+	printf("get a client,ip:[%s] , port:[%d]\n",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+
+	while(1){
+		read_size = read(client_socket_fd, read_buf, sizeof(read_buf));
+		if(read_size > 0){
+			printf("client:%s",read_buf);
+		}
+		write(client_socket_fd,write_buf,sizeof(write_buf));
+		sleep(1);
+	}
 
 	close(server_socket_fd);
 	close(client_addr_size);
